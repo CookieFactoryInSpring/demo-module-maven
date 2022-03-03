351b0ca262123716ea4011aa7ae3c0d8de112e08
Nassim Bounouas <nassim.bounouas@univ-cotedazur.fr>
2022-03-03 16:48:08 +0100

    Dependency greeting-app -> greeting-component

--- greeting-app/src/main/java/fr/univcotedazur/demomodulemaven/greetingapp/controllers/GreetingController.java
+++ greeting-app/src/main/java/fr/univcotedazur/demomodulemaven/greetingapp/controllers/GreetingController.java
@@ -1,5 +1,7 @@
 package fr.univcotedazur.demomodulemaven.greetingapp.controllers;
 
+import fr.univcotedazur.demomodulemaven.greetingapp.interfaces.Greeter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
+    @Autowired
+    private Greeter greeter;
+
     @GetMapping(path = "/")
     public ResponseEntity<String> greetVisitor() {
-        return ResponseEntity.ok("Hello world !");
+        return ResponseEntity.ok(greeter.greet("world"));
     }
 }
